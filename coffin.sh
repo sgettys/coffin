c3431eb9e1223a8f9d08efda10b7da3557756921
Steven Gettys <s.gettys@f5.com>
2022-10-09 11:22:48 -0700

    fixed outputs

--- coffin.sh
+++ coffin.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 # Sleep for 3 minutes to let fog machine warm up
-# Toggle outputs
-raspi-gpio set 24 op dh
-sleep 1
-raspi-gpio set 24 op dl
 sleep 180
 # start service loop
 while true
 do
   # Turn on fog machine
-  raspi-gpio set 23 op dh
+  raspi-gpio set 24 op dh
   sleep 1
-  raspi-gpio set 23 op dl
+  raspi-gpio set 24 op dl
   # let fog build up
   sleep 5
   # Start to open coffin
-  raspi-gpio set 24 op dh
+  raspi-gpio set 23 op dh
   sleep 5
   # Turn off fog
   raspi-gpio set 25 op dh
@@ -24,7 +20,7 @@ do
   # Keep coffin open for 30 seconds
   sleep 30
   # Close the coffin
-  raspi-gpio set 24 op dl
+  raspi-gpio set 23 op dl
   # Wait for a minute + close time (~10 seconds)
   sleep 70
 done
